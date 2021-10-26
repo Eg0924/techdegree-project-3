568a98a39e09813fe8d555331e3d0a82ee0ff36d
Eddy Gutierrez <eddytiger17@yahoo.com>
2021-10-25 23:52:06 -0400

    Update script.js

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -28,7 +28,8 @@ const shirtColor = document.getElementById('color');
 
 
 shirtColor.disabled = true;
-
+// Event listener to look for a change in the selection on the design dropdown menu.
+//Once the selection is made the colors for the selection appear on the next box below.
 shirtDesign.addEventListener('change', (e)=>{
     shirtColor.disabled = false;
     for(let i = 0; i<shirtColor.children.length; i++){
@@ -43,9 +44,29 @@ shirtDesign.addEventListener('change', (e)=>{
         shirtColor.children[i].selected = false; 
        }
     }
-
-   
-   
-   
 });
 
+// Register for activities section 
+// variables to select the activities checkboxes and the cost section
+const regForAct = document.getElementById('activities');
+const totalForAct = document.getElementById('activities-cost');
+
+console.log(regForAct);
+let totalCost = 0;
+// event listener to store the value of the activity clicked by user!
+regForAct.addEventListener('change', (e)=>{
+    //const cost = parseInt(e.target.getAttribute('data-cost'));
+    let cost = e.target.getAttribute('data-cost');
+    cost = +cost;
+   // console.log(cost, typeof cost);
+    
+   if(e.target.checked){
+    totalCost += cost;
+   }else if(e.target.checked = false){
+       totalCost -= cost;
+   }
+
+
+    console.log(totalCost);
+    console.log(e.target.checked);
+});
